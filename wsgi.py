cda1182ba3ab2a7d70d8296afeafc8b98960f2a2
khoa Nguyễn Nhật <khoanguyennhat@khoas-MacBook-Pro-2.local>
2024-02-27 14:47:23 +0700

    add wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,10 @@
 from src import create_app
 
-app = create_app()
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-    
+    app = create_app()
     parser = ArgumentParser()
     parser.add_argument('-p', '--port', default= 2025, type= int, help = 'port to start')
     args = parser.parse_args()
