6710196b2c8a9542b7ca4b66d3845dd9a17f3ef9
khoa Nguyễn Nhật <khoanguyennhat@khoas-MacBook-Pro-2.local>
2024-03-04 12:08:21 +0700

    audit file name

--- src/api/custom_gpt/routes/routes.py
+++ src/api/custom_gpt/routes/routes.py
@@ -15,11 +15,8 @@ class CustomGPTAPI(Resource):
             body = request.json
             saved_file_path = os.path.join(os.path.join(os.getcwd(), TMP_FOLDER), body['name'])
             result = upload_file_to_customgpt(body['name'], saved_file_path)
-            return {
-                "data": result.__dict__,
-                "status": 200,
-                "message": "Upload File successful"
-            }
+            return result
+            
         except Exception as error:
             return {
                 "data": None,
